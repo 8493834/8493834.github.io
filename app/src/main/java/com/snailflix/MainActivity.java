724f3a7c76e6dbefa02c5fec46f72b292f03153b
Joshua coding.inc <joshua.steel.joshua@gmail.com>
2026-08-03 18:08:36 +1000

    Update MainActivity.java

--- app/src/main/java/com/snailflix/MainActivity.java
+++ app/src/main/java/com/snailflix/MainActivity.java
@@ -25,21 +25,10 @@ public class MainActivity extends AppCompatActivity {
         // Fixes crashes on Chromebooks with hardware acceleration especialy you otis, wait why am i puting a converation in a comment nobody even reads these!!
         webView.setLayerType(WebView.LAYER_TYPE_HARDWARE, null);
 
-    
+        // Ensures links open inside the WebView instead of external browsers
         webView.setWebViewClient(new WebViewClient());
 
-       
+        // Correct target URL
         webView.loadUrl("https://8493834.github.io/sps/snailflix.html");
-    }
-}
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
     }
 }
