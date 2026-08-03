d1dad69891382caa0c24644c79743f680dd18598
Joshua coding.inc <joshua.steel.joshua@gmail.com>
2026-08-03 18:05:25 +1000

    Update MainActivity.java

--- app/src/main/java/com/snailflix/MainActivity.java
+++ app/src/main/java/com/snailflix/MainActivity.java
@@ -1,4 +1,4 @@
-package com.snailflix.tv;
+package com.snailflix;
 
 import android.os.Bundle;
 import android.webkit.WebSettings;
@@ -7,22 +7,31 @@ import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         webView = new WebView(this);
         setContentView(webView);
 
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setMediaPlaybackRequiresUserGesture(false);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        
+        // Fixes crashes on Chromebooks with hardware acceleration especialy you otis, wait why am i puting a converation in a comment nobody even reads these!!
+        webView.setLayerType(WebView.LAYER_TYPE_HARDWARE, null);
 
+    
         webView.setWebViewClient(new WebViewClient());
+
+       
         webView.loadUrl("https://8493834.github.io/sps/snailflix.html");
+    }
+}
     }
 
     @Override
